20b20655ec8f31e70ab25b6e9677296cae6e62c8
Tchollo88 <Tcholloman@gmail.com>
2024-05-07 08:08:30 -0700

    Add project files.

---
+++ MultiUserBank_Holloman_Project/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MultiUserBank_Holloman_Project
+{
+    /*Questions
+        Does the key logging in the menies need to be specifically numbers? 
+        Or can I continue to use key logging like I have been?
+        Can we use multiple classes like what we have covered this week? 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
